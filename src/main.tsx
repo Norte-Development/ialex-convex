fbd4bf1c985fe0e4c79292ac4902da8bf62e626e
Agustin Irala PC <agustinirala240@gmail.com>
2025-07-08 15:13:46 -0300

    Simple fix on convex erros

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
+// import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+// import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
 import "./index.css";
 import App from "./App.tsx";
-import { ErrorBoundary } from "./ErrorBoundary.tsx";
+// import { ErrorBoundary } from "./ErrorBoundary.tsx";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ErrorBoundary>
+    {/* <ErrorBoundary>
       <ClerkProvider
         publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
-      >
-        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-          <App />
-        </ConvexProviderWithClerk>
+      > */}
+    {/* <ConvexProviderWithClerk client={convex} useAuth={useAuth}> */}
+    <App />
+    {/* </ConvexProviderWithClerk>
       </ClerkProvider>
-    </ErrorBoundary>
+    </ErrorBoundary> */}
   </StrictMode>,
 );
