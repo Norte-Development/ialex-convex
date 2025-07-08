99b9e1a0e42995677b25640ebfb9f1fbd885bb23
Agustin Irala PC <agustinirala240@gmail.com>
2025-07-08 16:13:03 -0300

    Basic home page, only UI

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -9,21 +9,16 @@ import {
 
 export default function NavBar() {
   return (
-    <nav className="flex px-5  justify-between items-center h-9 w-full bg-white border-b border-gray-200 text-black shadow-md">
+    <nav className="flex px-5 justify-between items-center h-9 w-full bg-background text-foreground border-b border-border shadow-md">
       <div className="flex gap-4">
-        <Settings color="black" className="cursor-pointer" size={20} />
-        <CircleUserRound color="black" className="cursor-pointer" size={20} />
+        <Settings className="cursor-pointer" size={20} />
+        <CircleUserRound className="cursor-pointer" size={20} />
       </div>
       <div className="flex gap-4">
-        <FileSearch2 color="black" className="cursor-pointer" size={20} />
-        <UserIcon
-          color="black"
-          fill="black"
-          className="cursor-pointer"
-          size={20}
-        />
-        <TvMinimalPlay color="black" className="cursor-pointer" size={20} />
-        <UsersRound color="black" className="cursor-pointer" size={20} />
+        <FileSearch2 className="cursor-pointer" size={20} />
+        <UserIcon fill="currentColor" className="cursor-pointer" size={20} />
+        <TvMinimalPlay className="cursor-pointer" size={20} />
+        <UsersRound className="cursor-pointer" size={20} />
       </div>
     </nav>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,38 @@
+import { Textarea } from "@/components/ui/textarea";
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <section className="flex flex-col min-h-screen bg-white justify-center items-center relative">
+      {/* Mancha azul decorativa */}
+      <div className="absolute -bottom-20 -left-20 w-72 h-72 bg-blue-500 rounded-full blur-3xl opacity-30 z-10"></div>
+
+      <div className="w-1/2 flex flex-col gap-4 items-center justify-center">
+        <div className="flex flex-col gap-8 w-full justify-center items-center">
+          <h1 className=" font-poppins  font-bold lg:text-4xl text-xl">
+            {" "}
+            ¡Bienvenido/a a iAlex!
+          </h1>
+          <Textarea
+            placeholder=" ¿En qué trabajamos hoy?"
+            className="min-h-[100px] max-h-[250px] overflow-y-auto"
+          />
+        </div>
+        <div className="flex w-full  ">
+          <div className="w-1/2 text-[#406df5] flex flex-col justify-center items-center">
+            <p className="font-bold text-xl">Como preguntarle a la IA</p>
+            <ul className="list-disc pl-5 marker:text-[#406df5]">
+              <li>preguntas comunes</li>
+              <li>biblioteca de prompts</li>
+            </ul>
+          </div>
+          <div className="w-1/2 text-[#406df5] flex flex-col justify-center items-center">
+            <p className="font-bold text-xl">Como preguntarle a la IA</p>
+            <ul className="list-disc pl-5 marker:text-[#406df5]">
+              <li>preguntas comunes</li>
+              <li>biblioteca de prompts</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
