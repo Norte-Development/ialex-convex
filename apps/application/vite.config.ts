5c82be20dfe53a8fe4c3d5b501475354ec00c28f
tymkiwdylan <tymkiwdylan@gmail.com>
2025-08-25 11:24:26 -0300

    vite config

--- apps/application/vite.config.ts
+++ apps/application/vite.config.ts
@@ -27,6 +27,9 @@ export default defineConfig({
     },
     chunkSizeWarningLimit: 1000, // Increase warning limit to 1MB
   },
+  preview: {
+    allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', '192.168.1.100','y8kos4kc0wgowocowk8cogg4.31.97.20.213.sslip.io', 'app.ialex.com.ar', 'ialex.com.ar', 'app2.ialex.com.ar' ],
+  },
   optimizeDeps: {
     include: ['react', 'react-dom', 'convex'],
   },
