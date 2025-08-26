fb89622c19a251d47798f3797538a5a9c3130264
tymkiwdylan <tymkiwdylan@gmail.com>
2025-08-26 09:50:25 -0300

    attempt at using nginx for serving real static server

--- apps/application/vite.config.ts
+++ apps/application/vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000, // Increase warning limit to 1MB
   },
   preview: {
-    allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', '192.168.1.100','y8kos4kc0wgowocowk8cogg4.31.97.20.213.sslip.io', 'app.ialex.com.ar', 'ialex.com.ar', 'app2.ialex.com.ar' ],
+    allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', '192.168.1.100','y8kos4kc0wgowocowk8cogg4.31.97.20.213.sslip.io', 'app.ialex.com.ar', 'ialex.com.ar', 'app2.ialex.com.ar', 'nortedev.net', 'app.nortedev.net' ],
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'convex'],
