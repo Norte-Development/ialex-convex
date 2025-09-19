4e59452b7b38b3a0586c4cc6ab32fa42d0a757ef
tymkiwdylan <tymkiwdylan@gmail.com>
2025-09-19 09:11:24 -0300

    minor changes

--- apps/application/src/components/Agent/egislation-modal.tsx
+++ apps/application/src/components/Agent/egislation-modal.tsx
@@ -1,7 +1,6 @@
 import {
     Dialog,
     DialogContent,
-    DialogTrigger,
   } from "@/components/ui/dialog"
 import { NormativeDetails } from "@/components/DataBase/NormativeDetails";
 import { useAction } from "convex/react";
@@ -14,11 +13,10 @@ export function LegislationModal({ open, setOpen, normativeId }: { open: boolean
 
 
     return (
-        <Dialog open={open} onOpenChange={setOpen}>
-  <DialogTrigger>Open</DialogTrigger>
-  <DialogContent>
-    <NormativeDetails jurisdiction="py" id={normativeId} getNormativeAction={getNormativeAction} />
-  </DialogContent>
-</Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <NormativeDetails jurisdiction="py" id={normativeId} getNormativeAction={getNormativeAction} />
+      </DialogContent>
+    </Dialog>   
     )
 }
