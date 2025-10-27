2e79ac33b3c8d4ff89015ac3032bce2392938bc4
Irala Agustin <damian.irala@outlook.com>
2025-10-27 09:01:15 -0300

    Fix shared file

--- packages/shared/src/tiptap/schema.ts
+++ packages/shared/src/tiptap/schema.ts
@@ -1,12 +1,19 @@
 import StarterKit from "@tiptap/starter-kit";
 import TextAlign from "@tiptap/extension-text-align";
 import Underline from "@tiptap/extension-underline";
-import {TextStyle} from "@tiptap/extension-text-style";
+import {
+  Color,
+  FontFamily,
+  FontSize,
+  LineHeight,
+  TextStyle,
+} from "@tiptap/extension-text-style";
 import Image from "@tiptap/extension-image";
 import { Table } from "@tiptap/extension-table";
 import { TableRow } from "@tiptap/extension-table-row";
 import { TableHeader } from "@tiptap/extension-table-header";
 import { TableCell } from "@tiptap/extension-table-cell";
+import Link from "@tiptap/extension-link";
 import { InlineChange, BlockChange, LineBreakChange } from "./changeNodes";
 import { getSchema } from "@tiptap/core";
 import { Schema } from "@tiptap/pm/model";
@@ -20,6 +27,20 @@ export function buildServerSchema(): Schema {
     InlineChange,
     BlockChange,
     LineBreakChange,
+    LineHeight,
+    Link.configure({
+      openOnClick: false,
+      autolink: true,
+      defaultProtocol: "https",
+      protocols: ["http", "https"],
+    }),
+    Color,
+    FontFamily.configure({
+      types: ["textStyle"],
+    }),
+    FontSize.configure({
+      types: ["textStyle"],
+    }),
     TextAlign.configure({ types: ["heading", "paragraph"] }),
     Underline,
     // Image extension
@@ -41,5 +62,3 @@ export function buildServerSchema(): Schema {
   ];
   return getSchema(extensions);
 }
-
-
